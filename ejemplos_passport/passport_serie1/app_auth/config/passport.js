818ef2bb3f07d9e7bc01a4766ebfdec98a463d5c
AGC <agalveca@gmail.com>
2014-05-02 01:51:43 +0200

    aurora creada aplicaciones ejemplos_passport se ha trasladado a ella el primer ejemplo de la serie

--- ejemplos_passport/passport_serie1/app_auth/config/passport.js
+++ ejemplos_passport/passport_serie1/app_auth/config/passport.js
@@ -1,12 +1,12 @@
 // config/passport.js
 
-// load all the things we need
+
 var LocalStrategy   = require('passport-local').Strategy;
 
-// load up the user model
+
 var User       		= require('../models/user');
 
-// expose this function to our app using module.exports
+
 module.exports = function(passport) {
 
     // =========================================================================
@@ -15,12 +15,12 @@ module.exports = function(passport) {
     // required for persistent login sessions
     // passport needs ability to serialize and unserialize users out of session
 
-    // used to serialize the user for the session
+
     passport.serializeUser(function(user, done) {
         done(null, user.id);
     });
 
-    // used to deserialize the user
+
     passport.deserializeUser(function(id, done) {
         User.findById(id, function(err, user) {
             done(err, user);
