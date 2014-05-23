4cd2b1283abd550391fcf90a976793d05735b2d2
agc en Nova <agc@pnte.cfnavarra.es>
2014-05-23 16:35:11 +0200

    N añadido sequelize-_mysql a ang-mysql

---
+++ ang-mysql/routes/sequelize_mysql.js
@@ -0,0 +1,48 @@
+var express     = require('express');
+var router      = express.Router();
+
+
+var Sequelize   = require('sequelize')
+
+    , sequelize = new Sequelize('todo_demo', 'usuariointraweb', '19601706', {
+        dialect: "mysql", // or 'sqlite', 'postgres', 'mariadb'
+        port:    3306    // or 5432 (for postgres)
+    })
+
+sequelize
+    .authenticate()
+    .complete(function(err) {
+        if (!!err) {
+            console.log('Unable to connect to the database:', err)
+        } else {
+            console.log('Connection has been established successfully.')
+        }
+    })
+
+var User    = sequelize.define('User', {
+    username: Sequelize.STRING,
+    password: Sequelize.STRING
+})
+
+var Todo    = sequelize.define('todo', {
+   title: Sequelize.STRING,
+
+})
+
+sequelize
+    .sync({ force: true })
+    .complete(function(err) {
+        if (!!err) {
+            console.log('An error occurred while creating the table:', err)
+        } else {
+            console.log('It worked!')
+        }
+    })
+
+
+router.get('/', function(req, res) {
+    res.send('Prueba de sequelize');
+})
+
+module.exports=router
+
