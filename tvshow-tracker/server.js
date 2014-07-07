87ad3c5da1f80a821901ba955a005bb6cc35349d
Armando Galve <agc@localhost.localdomain>
2014-07-07 23:44:38 +0200

    Zaio terminado el paso 11, aunque sin comprobar que funciona el correo

--- tvshow-tracker/server.js
+++ tvshow-tracker/server.js
@@ -4,12 +4,23 @@ var express             = require('express');
 var path                = require('path');
 var logger              = require('morgan');
 var cookieParser        = require('cookie-parser');
+
+
+
 var bodyParser          = require('body-parser');
 
-var async = require('async');
-var request = require('request');
-var xml2js = require('xml2js');
-var _ = require('lodash');
+var async               = require('async');
+var request             = require('request');
+var xml2js              = require('xml2js');
+
+var agenda = require('agenda')({ db: { address: 'localhost:27017/test' } });
+var sugar = require('sugar');
+var nodemailer = require('nodemailer');
+var _                   = require('lodash');
+
+var session = require('express-session');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 
 var agenda = require('agenda')({ db: { address: 'localhost:27017/test' } });
@@ -66,6 +77,32 @@ userSchema.methods.comparePassword = function(candidatePassword, cb) {
 var User = mongoose.model('User', userSchema);
 var Show = mongoose.model('Show', showSchema);
 
+passport.serializeUser(function(user, done) {
+    done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+    User.findById(id, function(err, user) {
+        done(err, user);
+    });
+});
+
+passport.use(new LocalStrategy({ usernameField: 'email' }, function(email, password, done) {
+    User.findOne({ email: email }, function(err, user) {
+        if (err) return done(err);
+        if (!user) return done(null, false);
+        user.comparePassword(password, function(err, isMatch) {
+            if (err) return done(err);
+            if (isMatch) return done(null, user);
+            return done(null, false);
+        });
+    });
+}));
+
+function ensureAuthenticated(req, res, next) {
+    if (req.isAuthenticated()) next();
+    else res.send(401);
+}
 mongoose.connect('localhost');
 
 var app = express();
@@ -75,7 +112,39 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 app.use(cookieParser());
+app.use(session({ secret: 'keyboard cat' }));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next) {
+    if (req.user) {
+        res.cookie('user', JSON.stringify(req.user));
+    }
+    next();
+});
+
+app.post('/api/login', passport.authenticate('local'), function(req, res) {
+    res.cookie('user', JSON.stringify(req.user));
+    res.send(req.user);
+});
+
+// no hay validacion para validar se usa express-validator
+app.post('/api/signup', function(req, res, next) {
+    var user = new User({
+        email: req.body.email,
+        password: req.body.password
+    });
+    user.save(function(err) {
+        if (err) return next(err);
+        res.send(200);
+    });
+});
+
+app.get('/api/logout', function(req, res, next) {
+    req.logout();
+    res.send(200);
+});
 
 app.get('/api/shows', function(req, res, next) {
     var query = Show.find();
@@ -167,8 +236,13 @@ app.post('/api/shows', function (req, res, next) {
                 }
                 return next(err);
             }
-           // var alertDate = Date.parse('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2});
-            var alertDate = 'Next ' + show.airsDayOfWeek + ' at ' + show.airsTime
+
+            // funciona porque sugar.js añade el metodo
+            var alertDate = Date.create('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2});
+            agenda.schedule(alertDate, 'send email alert', show.name).repeatEvery('1 week');
+
+            // var alertDate = Date.parse('Next ' + show.airsDayOfWeek + ' at ' + show.airsTime).rewind({ hour: 2});
+            //var alertDate = 'Next ' + show.airsDayOfWeek + ' at ' + show.airsTime
             agenda.schedule(alertDate, 'send email alert', show.name).repeatEvery('1 week');
             res.send(200);
         });
@@ -182,12 +256,85 @@ app.get('/api/shows/:id', function(req, res, next) {
     });
 });
 
+app.post('/api/subscribe', ensureAuthenticated, function(req, res, next) {
+    Show.findById(req.body.showId, function(err, show) {
+        if (err) return next(err);
+        show.subscribers.push(req.user.id);
+        show.save(function(err) {
+            if (err) return next(err);
+            res.send(200);
+        });
+    });
+});
+
+app.post('/api/unsubscribe', ensureAuthenticated, function(req, res, next) {
+    Show.findById(req.body.showId, function(err, show) {
+        if (err) return next(err);
+        var index = show.subscribers.indexOf(req.user.id);
+        show.subscribers.splice(index, 1);
+        show.save(function(err) {
+            if (err) return next(err);
+            res.send(200);
+        });
+    });
+});
+
+
 app.get('*', function(req, res) {
     res.redirect('/#' + req.originalUrl);
 });
 
 app.listen(app.get('port'), function() {
     console.log('Express server listening on port ' + app.get('port'));
+});
+
+agenda.define('send email alert', function(job, done) {
+    Show.findOne({ name: job.attrs.data }).populate('subscribers').exec(function(err, show) {
+        var emails = show.subscribers.map(function(user) {
+            return user.email;
+        });
+
+        var upcomingEpisode = show.episodes.filter(function(episode) {
+            return new Date(episode.firstAired) > new Date();
+        })[0];
+
+        /* este es el original
+        var smtpTransport = nodemailer.createTransport('SMTP', {
+            service: 'SendGrid',
+            auth: { user: 'hslogin', pass: 'hspassword00' }
+        });
+
+        */
+
+        var smtpTransport = nodemailer.createTransport('SMTP', {
+            service: 'Gmail',
+            auth: { user: 'agalveca@gmail.com', pass: '19601706' }
+        });
+
+        var mailOptions = {
+            from: 'Fred Foo ✔ <foo@blurdybloop.com>',
+            to: emails.join(','),
+            subject: show.name + ' is starting soon!',
+            text: show.name + ' starts in less than 2 hours on ' + show.network + '.\n\n' +
+                'Episode ' + upcomingEpisode.episodeNumber + ' Overview\n\n' + upcomingEpisode.overview
+        };
+
+        smtpTransport.sendMail(mailOptions, function(error, response) {
+            console.log('Message sent: ' + response.message);
+            smtpTransport.close();
+            done();
+        });
+    });
+});
+
+agenda.start();
+
+agenda.on('start', function(job) {
+    console.log("Job %s starting", job.attrs.name);
+});
+
+agenda.on('complete', function(job) {
+    console.log("Job %s finished", job.attrs.name);
 });
 
 
